d93429645d52ac0c25d259cc71ae8cdfdb0dc9b1
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2023-03-24 11:19:55 +0000

    describe() & fix test codes

--- src/testing/Counter.test.js
+++ src/testing/Counter.test.js
@@ -2,11 +2,30 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Counter from "./Counter";
 
-test("Counter increments and decrements when the buttons are clicked", () => {
-    render(<Counter />);
-    const count = screen.getByText("0");
-    const increase = screen.getByRole("button", { name: /increase/i });
-    const decrease = screen.getByRole("button", { name: /decrease/i });
+
+describe("Counter Test", () => {
+
+    let increaseBtn, decreaseBtn, count;
+
+    beforeEach(() => { //This is a function that runs before each test and runs with the same amount of tests; (2 tests = 2 runs)
+        render(<Counter />);
+        count = screen.getByText("0");
+        increaseBtn = screen.getByText("Increase");
+        decreaseBtn = screen.getByText("Decrease");
+    });
+
+    //afterEach() runs after each test
+
+    //also there is beforeAll() which runs before all tests for once
+    //afterAll() which runs after all tests for once
+
+    test("Counter increase when the increase button is clicked", () => {
+        userEvent.click(increaseBtn);
+        expect(count).toHaveTextContent("1");
+    });
     
-    expect(count).toBeInTheDocument();
+    test("Counter decrease when the decrease button is clicked", () => {
+        userEvent.click(decreaseBtn);
+        expect(count).toHaveTextContent("-1");
+    });
 });
